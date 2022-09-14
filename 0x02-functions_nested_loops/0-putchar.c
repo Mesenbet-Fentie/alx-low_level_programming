67b71b01e26c68817981458a5b435d89cf0bf223
Mesenbet-Fentie <messi.fentie@gmail.com>
2022-09-14 04:26:25 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
- * main - starts the program
+ * main - prints putchar
  *
- * Return: nothing if successful
+ * Return: always 0
  */
 int main(void)
-{
-	char string[] = "_putchar";
-	int ind = 0;
+{ 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-		while (string[ind])
-			if (ind <= 8)
-			{
-				_putchar(string[ind]);
-				ind++;
-			}
-		_putchar('\n');
-		return (0);
+	return (0);
 }
