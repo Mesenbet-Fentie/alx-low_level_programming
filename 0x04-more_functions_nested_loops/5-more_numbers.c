75768cc37852e27a77026494569b9c36a98e95bc
Mesenbet-Fentie <messi.fentie@gmail.com>
2022-09-15 04:04:33 -0700

    Prints 0 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numners from 0 to 14 10 times
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		int j;
+
+		for (j = 0; j <= 14; j++)
+			_putchar(j + '0');
+	_putchar('\n')
+}
