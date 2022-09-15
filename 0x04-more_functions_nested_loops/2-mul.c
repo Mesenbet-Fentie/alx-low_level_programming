d36cfeb4c7e6c3bede968f2c0ae0b5333ec5cec1
Mesenbet-Fentie <messi.fentie@gmail.com>
2022-09-15 03:01:51 -0700

    Multiplies two intigers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies wo intiger
+ *@a: first integer
+ *@b: second integer
+ *
+ * Return: nothing
+ */
+int mul(int a, int b)
+{
+	int x = a * b;
+
+	return (z);
+}
