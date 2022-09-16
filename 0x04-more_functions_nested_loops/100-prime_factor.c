5843154e470e78a72e410b117f9824c2b03d7449
Mesenbet-Fentie <messi.fentie@gmail.com>
2022-09-16 05:20:52 -0700

    Largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the maximum prime factor of number
+ *
+ * Return: nothing
+ */
+void main(void)
+{
+	long long num;
+	int maxPrime, oddPrime;
+
+	num = 612852475143;
+	maxPrime = 0;
+	oddPrime = 3;
+
+	while (num % 2 == 0)
+	{
+		maxPrime = 2;
+		num = num / 2;
+	}
+	while (num != 1)
+	{
+		while (num % oddPrime == 0)
+		{
+			maxPrime = oddPrime;
+			num = num / oddPrime;
+		}
+		oddPrime += 2;
+	}
+	printf("%d\n", maxPrime);
+}
