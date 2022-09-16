58722abbff82df0121eabacbda35eec29103f2cf
Mesenbet-Fentie <messi.fentie@gmail.com>
2022-09-16 06:27:31 -0700

    Posetive or negative number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if the number is posetive or negative 
+ * Returns: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	posetive_or_negative(i);
+
+	return(0);
+}
